033eb7bbe85d9350a0beb3a8a4a92d507ee87efb
PeterDaneholm <av28qm@student.aau.dk>
2024-03-05 11:11:09 +0100

    Clean up in routing, and added last routes

--- frontend/exam_webshop/src/App.jsx
+++ frontend/exam_webshop/src/App.jsx
@@ -9,6 +9,8 @@ import Login from './routes/Login';
 import Signup from './routes/Signup';
 import Profile from './routes/Profile';
 import CartProvider from './components/CartContext';
+import Signout from './routes/Signout';
+import Checkout from './routes/Checkout';
 
 
 function App() {
@@ -21,17 +23,16 @@ function App() {
           <Route path="/" element="" />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signout" element={<Signout />} />
           <Route path="/shop" element={<ProductsPage />} />
           <Route path="/shop/:id" element={<ProductPage />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/profile/:slug" element={<Profile />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/uploadproduct" element={<AdminProduct />} />
         </Routes>
       </CartProvider>
 
-      <div className='grid h-screen'>
-        {/* <Outlet /> */}
-      </div>
     </>
   )
 }
--- frontend/exam_webshop/src/main.jsx
+++ frontend/exam_webshop/src/main.jsx
@@ -2,85 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Errorpage from './Errorpage.jsx'
-import Login from './routes/Login.jsx'
-import Signup from './routes/Signup.jsx'
-import Profile from './routes/Profile.jsx'
-import ProductsPage from './routes/ProductsPage.jsx'
-import ProductPage from './routes/ProductPage.jsx'
-import Admin from './routes/Admin.jsx'
-import AdminProduct from './routes/AdminProduct.jsx'
-import { useNavigate } from 'react-router-dom'
-import Cookies from 'js-cookie'
-import { jwtDecode } from 'jwt-decode'
+import { BrowserRouter } from 'react-router-dom'
 
-const ProtectedRoute = ({ children, scopes }) => {
-  const navigate = useNavigate()
-  const token = Cookies.get('token');
 
-  if (!token) {
-    navigate("/login")
-    return null
-  }
-
-  const decodedToken = jwtDecode(token);
-
-  if (!scopes.some(scope => decodedToken.scopes.includes(scope))) {
-    navigate("/error");
-    return null
-  }
-
-  return children;
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Errorpage />,
-    children: [
-      {
-        path: "/shop",
-        element: <ProductsPage />
-      },
-      {
-        path: "/shop/:id",
-        element: <ProductPage />
-      },
-      {
-        path: "/profile/:username",
-        element: <Profile />
-      },
-      {
-        path: "/admin",
-        element: <Admin />,/* (
-          <ProtectedRoute scopes={['admin']}>
-            <AdminProduct />
-          </ProtectedRoute>
-        ) */
-        children: [
-          {
-            path: "/admin/uploadproduct",
-            element: <AdminProduct />/* (
-              <ProtectedRoute scopes={['admin']}>
-                <AdminProduct />
-              </ProtectedRoute>
-            ) */
-          },
-        ]
-      },
-    ]
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/signup",
-    element: <Signup />
-  },
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
