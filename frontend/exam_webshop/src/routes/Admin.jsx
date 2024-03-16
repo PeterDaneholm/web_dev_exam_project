4c480ae3c0eda01b36efcc327e88f0be03b619b9
PeterDaneholm <av28qm@student.aau.dk>
2024-03-16 09:12:19 +0100

    Updates to admin page, conditional rendering for differetn subpage

--- frontend/exam_webshop/src/routes/Admin.jsx
+++ frontend/exam_webshop/src/routes/Admin.jsx
@@ -6,11 +6,14 @@ import { FaUsers } from "react-icons/fa";
 import api from '../api';
 import { onAdminRouteLoad } from '../utilities/get_user';
 import { useNavigate } from 'react-router-dom'
+import { MdEditSquare } from "react-icons/md";
+
 
 
 const Admin = () => {
     const [users, setUsers] = useState([])
     const [orders, setOrders] = useState([])
+    const [products, setProducts] = useState([])
     const [activeSubPage, setActiveSubPage] = useState("dashboard")
     const navigate = useNavigate()
 
@@ -25,29 +28,40 @@ const Admin = () => {
             //console.log(response)
             setUsers(response.data)
         }
-        fetchUsers()
-
         const getOrders = async () => {
             const response = await api.get("/orders", {
                 withCredentials: true,
             })
             setOrders(response.data)
         }
+        const getUsers = async () => {
+            const response = await api.get("/users", {
+                withCredentials: true
+            })
+            setUsers(response.data)
+        }
+
+        fetchUsers()
         getOrders()
+        getUsers()
 
     }, [])
+
+    function isActive(page) {
+        return activeSubPage === page ? "bg-contrastdark w-full " : ""
+    }
 
 
     return (
         <div className='flex flex-row'>
-            <div className='bg-gray-200 sticky h-screen w-44 mr-4 p-2 gap-4 max-w-screen'>
+            <div className='bg-gray-200 sticky h-screen w-44 mr-4 gap-4 max-w-screen'>
 
                 <ul className='mt-12 gap-2 grid text-xl'>
-                    <li className={activeSubPage === "dashboard" ? "bg-contrastdark" : ""} onClick={() => setActiveSubPage('dashboard')}>
+                    <li className={`${isActive("dashboard")} pl-2 hover:cursor-pointer`} onClick={() => setActiveSubPage('dashboard')}>
                         <MdOutlineDashboard className='inline' /> Dashboard</li>
-                    <li className={activeSubPage === "products" ? "bg-contrastdark" : ""} onClick={() => setActiveSubPage("products")}>
+                    <li className={`${isActive("products")} pl-2 hover:cursor-pointer`} onClick={() => setActiveSubPage("products")}>
                         <FaBasketShopping className='inline' /> Products</li>
-                    <li className={activeSubPage === "users" ? "bg-contrastdark" : ""} onClick={() => setActiveSubPage("users")}>
+                    <li className={`${isActive("users")} pl-2 hover:cursor-pointer`} onClick={() => setActiveSubPage("users")}>
                         <FaUsers className='inline' /> Users</li>
                     <li>
                         <Link to={"uploadproduct"}>Upload</Link>
@@ -57,23 +71,45 @@ const Admin = () => {
 
             <div className='m-6 p-3 bg-slate-200 w-screen h-auto rounded-xl shadow-sm'>
                 {activeSubPage === "dashboard" && <Dashboard />}
-                {activeSubPage === "products" && <Products />}
-                {activeSubPage === "users" && <Users />}
+                {activeSubPage === "products" && <Products content={products} />}
+                {activeSubPage === "users" && <Users content={users} />}
 
             </div>
-
         </div >
     )
 }
 
-function Dashboard() {
-    return <div>Dashboard</div>
+function Dashboard({ }) {
+    return <div>Dashboard
+        Need to figure out what to put here :/
+    </div>
 }
-function Products() {
-    return <div>Products</div>
+function Products({ content }) {
+    return <div>
+        <h3>All Products</h3>
+        {content.map(item => <div>
+            {item.name}
+
+            <p>
+                <MdEditSquare />
+            </p>
+        </div>)}
+    </div>
 }
-function Users() {
-    return <div>Users</div>
+function Users({ content }) {
+    return <div>
+        <h3>Total Users</h3>
+        <h4>{content.length}</h4>
+
+        <p>
+            {content.map((item) => (
+                <div key={item.id}>
+                    {item.username}
+                </div>
+            ))}
+        </p>
+
+    </div>
 }
 
 export default Admin
