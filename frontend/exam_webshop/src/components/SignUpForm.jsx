adf53e3627d38664b00ad59cfc0e3c17c1158fc5
PeterDaneholm <av28qm@student.aau.dk>
2024-03-18 08:49:32 +0100

    Update to toasts

--- frontend/exam_webshop/src/components/SignUpForm.jsx
+++ frontend/exam_webshop/src/components/SignUpForm.jsx
@@ -3,6 +3,7 @@ import api from '../api'
 import { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { Link } from 'react-router-dom'
+import { useToast } from './Toast/ToastContext'
 
 
 const SignUpForm = () => {
@@ -15,6 +16,7 @@ const SignUpForm = () => {
     password: '',
     role: 'user',
   })
+  const { ShowToast } = useToast()
 
   const handleChange = async (e) => {
     setFormData((prev) => {
@@ -36,6 +38,7 @@ const SignUpForm = () => {
       email_address: '',
       password: '',
     })
+    ShowToast(`User: ${formData.username} created`, "success")
   }
 
   return (
--- frontend/exam_webshop/src/components/Toast/Toast.jsx
+++ frontend/exam_webshop/src/components/Toast/Toast.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { PiWarningOctagonBold } from "react-icons/pi";
+import { FaRegCircleXmark } from "react-icons/fa6";
+
+
 
 const Toast = ({ label, status, closeToast }) => {
+    let icon = null;
 
     useEffect(() => {
         const timer = setTimeout(closeToast, 5000);
@@ -10,23 +16,25 @@ const Toast = ({ label, status, closeToast }) => {
     const bgColor = (status) => {
         switch (status) {
             case 'success':
+                icon = <IoMdCheckmarkCircleOutline />
                 return 'bg-green-500';
             case 'fail':
+                icon = <FaRegCircleXmark />
                 return 'bg-red-500';
+            case 'warning':
+                icon = <PiWarningOctagonBold />
+                return 'bg-orange-500';
             default:
                 return 'bg-slate-300';
         }
     }
 
-    const autoClose = () => {
-
-    }
-
     return (
         <div className={`${bgColor(status)} bg-opacity-90
-        absolute w-56 h-12 rounded-md text-lg my-auto flex items-center justify-center float-left top-36 right-12 z-20 border-2 border-gray-500 animate-fade-in-out`}
+        absolute w-56 h-12 rounded-md text-lg my-auto flex items-center float-left top-36 right-12 z-20 border-2 border-gray-500 animate-fade-in-out`}
         >
-            {label}
+            <span className='ml-2'>{icon}</span>
+            <span className='ml-4'>{label}</span>
         </div>
     )
 }
